bb9d8e7ea2ab821d8587bd19b13606e17d80f8f2
Ryo Chikazawa <chikathreesix@gmail.com>
2014-12-12 00:39:14 +0800

    Move parser.js to lib directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,5 +73,5 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-connect');
 
   grunt.registerTask('default', ['shell', 'compass', 'parse']);
-  grunt.loadTasks('tasks');
+  grunt.loadTasks('lib');
 }
